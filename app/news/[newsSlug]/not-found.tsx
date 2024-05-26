3e5983ce6653b4199a79d25406da72036784392f
garold664 <garold664@gmail.com>
2024-05-26 21:54:01 +0300

    created not found pages

---
+++ app/news/[newsSlug]/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found ☹️</h1>
+      <p>We could not find the article you were looking for.</p>
+    </div>
+  );
+}
