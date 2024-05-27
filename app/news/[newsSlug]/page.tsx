e6dd334816c7d83fc4c70afb968b3751aea3b20a
garold664 <garold664@gmail.com>
2024-05-27 16:39:26 +0300

    set up full view for images

--- app/news/[newsSlug]/page.tsx
+++ app/news/[newsSlug]/page.tsx
@@ -1,4 +1,5 @@
 import { DUMMY_NEWS } from '@/dummy-news';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -16,7 +17,12 @@ export default function NewsDetailPage({
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt="" />
+        <Link href={`/news/${params.newsSlug}/image`}>
+          <img
+            src={`/images/news/${newsItem.image}`}
+            alt={`${newsItem.title}`}
+          />
+        </Link>
         <h1>{newsItem.title}</h1>
         <time dateTime={newsItem.date}>{newsItem.date}</time>
       </header>
