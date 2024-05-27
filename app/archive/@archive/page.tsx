80c71c6e4adb099e6570d1a4717204df9bdd5f81
garold664 <garold664@gmail.com>
2024-05-27 10:34:06 +0300

    added helper functions

---
+++ app/archive/@archive/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ArchivePage() {
+  return (
+    <header className="archive-header">
+      <nav>
+        <ul></ul>
+      </nav>
+    </header>
+  );
+}
