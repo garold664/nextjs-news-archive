57193ab3d321247f16700193e01144e5ac082fd9
garold664 <garold664@gmail.com>
2024-05-30 10:27:07 +0300

    implemented client-side data fetching from backend

--- app/(content)/news/page.tsx
+++ app/(content)/news/page.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 import NewsList from '@/components/news-list';
+import { NewsItem } from '@/dummy-news';
 
-import { DUMMY_NEWS } from '@/dummy-news';
+// import { DUMMY_NEWS } from '@/dummy-news';
 // import Image from 'next/image';
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string>(null);
+  const [news, setNews] = useState<NewsItem[]>(null);
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setError('Failed to fetch news');
+        setIsLoading(false);
+      }
+
+      const news = await response.json();
+      setNews(news);
+      setIsLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {news && news.length > 0 && <NewsList news={news} />}
     </>
   );
 }
